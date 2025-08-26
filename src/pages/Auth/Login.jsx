beee028fc9ff6b2166f4c1d5bbbb72d381ce42de
Md Al-Amin <mdalamin.iubian@gmail.com>
2025-08-27 05:53:25 +0600

    added signup page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router';
 
 const Login = () => {
     return (
         <div className='min-h-screen flex justify-center items-center'>
-            <div className="card bg-base-100 w-full max-w-[750px] shrink-0 border border-[#db621f10] shadow-2xl py-15">
-                <div className="card-body space-y-10">
+            <div className="card bg-base-100 w-full max-w-[750px] shrink-0 border border-[#db621f10] shadow-2xl">
+                <div className="card-body space-y-10 py-20">
                     <h1 className='text-4xl font-semibold text-center text-gray-800'>Login Your Account</h1>
-                    <form onSubmit={''} className="fieldset px-25 space-y-3">
+                    <form onSubmit={''} className="fieldset sm:px-20 space-y-2">
                         {/* email  */}
                         <label className="label font-semibold text-xl text-gray-800">Email Address</label>
                         <input required name='email' type="email" className="input w-full" placeholder="Enter Your Email Address" />
@@ -23,6 +24,13 @@ const Login = () => {
                         {/* submit button  */}
                         <button type='submit' className="btn btn-neutral mt-4 bg-gray-800">Login</button>
                     </form>
+                    <div className='flex items-center justify-between text-gray-800'>
+                        <p ><hr /></p>
+                        <p className='text-center font-medium'>or</p>
+                        <p><hr /></p>
+                    </div>
+
+                    <p className='flex items-center justify-center gap-2 text-gray-600 font-semibold'><FcGoogle size={25} /> <span>Login with Google</span></p>
                     <p className='text-center font-semibold text-gray-600'>Don't Have An Account? <Link to={'/signup'} className='text-[#db621f]'>Signup</Link></p>
                 </div>
             </div>
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { Link } from 'react-router';
 
 const Signup = () => {
     return (
-        <div>
-            
+        <div className='min-h-screen flex justify-center items-center'>
+            <div className="card bg-base-100 w-full max-w-[750px] shrink-0 border border-[#db621f10] shadow-2xl py-10">
+                <div className="card-body space-y-5">
+                    <h1 className='text-4xl font-semibold text-center text-gray-800'>Sign Up Your Account</h1>
+                    <form onSubmit={''} className="fieldset sm:px-20 space-y-1">
+
+                        {/* name  */}
+                        <label className="label font-semibold text-xl text-gray-800">Name</label>
+                        <input required name='name' type="text" className="input w-full" placeholder="Enter Your Name" />
+
+                        {/* email  */}
+                        <label className="label font-semibold text-xl text-gray-800">Email Address</label>
+                        <input required name='email' type="email" className="input w-full" placeholder="Enter Your Email Address" />
+
+                        {/* photo url  */}
+                        <label className="label font-semibold text-xl text-gray-800">Photo Url</label>
+                        <input required name='photo_url' type="text" className="input w-full" placeholder="Enter Your Photo url" />
+
+                        {/* password  */}
+                        <label className="label font-semibold text-xl text-gray-800">Password</label>
+                        <input required name='password' type="password" className="input w-full" placeholder="Enter Your Password" />
+
+                        <div><a className="link link-hover text-gray-600">Forgot password?</a></div>
+                        {/* error message  */}
+
+                        {/* {error && <p className="text-error">${error}</p>} */}
+
+                        {/* submit button  */}
+                        <button type='submit' className="btn btn-neutral mt-4 bg-gray-800">Sign up</button>
+                    </form>
+
+                    <div className='flex items-center justify-between text-gray-800'>
+                        <p ><hr /></p>
+                        <p className='text-center font-medium'>or</p>
+                        <p><hr /></p>
+                    </div>
+
+                    <p className='flex items-center justify-center gap-2 text-gray-600 font-semibold'><FcGoogle size={25} /> <span>Login with Google</span></p>
+
+                    <p className='text-center font-semibold text-gray-600'>Already Have An Account? <Link to={'/login'} className='text-[#db621f]'>Login</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
