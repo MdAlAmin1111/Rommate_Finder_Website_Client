62eb0521599708f6a0840c1198dbf9e7c8ccc7fa
Md Al-Amin <mdalamin.iubian@gmail.com>
2025-09-02 06:51:07 +0600

    implement React Awesome Reveal Library in SectionDivide

--- src/components/SectionDivider.jsx
+++ src/components/SectionDivider.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-// import { Fade } from 'react-awesome-reveal';
+import { Fade } from 'react-awesome-reveal';
+
 
 const SectionDivider = ({ text }) => {
     return (
         <div className='bg-[#C85A3C] text-base-100 transition-colors duration-300'>
             <div className='container mx-auto py-20 font-medium'>
-                {/* <Fade delay={300} duration={800} triggerOnce={false}> */}
-                <h1 className='text-4xl md:text-5xl text-center'>{text}</h1>
-                {/* </Fade> */}
+                <Fade delay={300} duration={800} direction='right' triggerOnce={false}>
+                    <h1 className='text-4xl md:text-5xl text-center'>{text}</h1>
+                </Fade>
             </div>
         </div>
     );
